8d33a8d17bd8bc3198ffd98a3b508ab45bdecf9b
Fernando Mendoza <fernandoza@gmail.com>
2020-05-19 15:05:08 -0500

    feat: create a test for arrays

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
@@ -8,4 +12,22 @@ public class MyFirstTest {
         assertEquals("Codeup", company);
         assertNotEquals("CodeUp", company);
     }
+
+    @Test
+    public void testIfListsAreNotTheSame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void testIfArraysAreEqual(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+        assertArrayEquals(numbers, otherNumbers);
+    }
+
 }
