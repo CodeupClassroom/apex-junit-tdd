a52fb819aa3158492fa18f3f8dd4f8c212c60ba0
Fernando Mendoza <fernandoza@gmail.com>
2020-05-19 14:00:59 -0500

    feat: initial structure for a junit project

---
+++ src/test/java/TestLecture.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestLecture {
+
+    @Test
+    public void myFirstTest(){
+        String company = "Codeup";
+        assertEquals("Codeup", company);
+//        assertEquals("codeup", company);
+    }
+
+    // When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+
+    }
+
+}
